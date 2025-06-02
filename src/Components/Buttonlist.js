0944e5a0f325a7dc52d60ca1f93e2731ee47efcb
kgan3008 <kalash.gangwal@7-11.com>
2025-06-02 19:56:22 +0530

    Basic setup of youtube app (High level and low-level design)

---
+++ src/Components/Buttonlist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Buttonlist = ()=>{
+
+    return(
+        <div>
+           Buttonlist
+        </div>
+    )
+}
+
+export default Buttonlist;
---
+++ src/Components/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const VideoCard = ()=>{
+
+    return(
+        <div>
+          VideoCard
+        </div>
+    )
+}
+
+export default VideoCard;
